1cc458efc2c60d117309a3685d8b7122f8f2672f
Troy <168228319+Yort0@users.noreply.github.com>
2024-04-29 15:48:02 -0500

    Update script.js
    v4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+inputContainer.focus(); // Try adding this line
+
 const processButton = document.getElementById('processButton');
 const inputContainer = document.getElementById('inputContainer');
 const outputArea = document.getElementById('tableOutput');
