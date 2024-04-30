dd5cb0bc1c788899624c8ec3303e416193bee85c
Troy <168228319+Yort0@users.noreply.github.com>
2024-04-29 23:15:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const processButton = document.getElementById('processButton');
-const inputArea = document.getElementById('tableInput');
+const inputArea = document.getElementById('inputContainer');
 const outputArea = document.getElementById('tableOutput');
 
 processButton.addEventListener('click', function() {
-    const rawTable = inputArea.value;
+    const rawTable = inputArea.innerHTML;
     const processedTable = processAndDisplayTable(rawTable);
     outputArea.innerHTML = processedTable;
 });
 
 function processAndDisplayTable(tableText) {
-    const rows = tableText.split('\n');
+    const rows = tableText.trim().split('<div><br></div>'); // Adjust for line breaks
     let newTableHTML = '<table style="border: 1px solid black; font-size: 0.8rem;">'; // Table style with font size
 
     for (let i = 0; i < rows.length; i++) {
         newTableHTML += '<tr>';
         const cells = rows[i].split('\t');
         cells.forEach(cell => {
-            newTableHTML += `<td>${cell} <button onclick="deleteRow(this)">&#10006;</button></td>`;  // Add delete button
+            newTableHTML += `<td>${cell}</td>`;
         });
+        newTableHTML += `<td><button class="deleteButton" onclick="deleteRow(this)">&#10006;</button></td>`; // Add delete button
         newTableHTML += '</tr>';
     }
 
@@ -26,6 +27,6 @@ function processAndDisplayTable(tableText) {
 }
 
 function deleteRow(deleteButton) {
-    const tableRow = deleteButton.parentElement.parentElement; // Get the table row
+    const tableRow = deleteButton.parentNode.parentNode; // Get the table row
     tableRow.parentNode.removeChild(tableRow); // Remove the row from the table
 }
