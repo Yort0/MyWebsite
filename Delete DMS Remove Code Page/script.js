a77ca707500c46affecbf78a93a18f776e00d692
Troy <168228319+Yort0@users.noreply.github.com>
2024-05-28 16:55:00 -0500

    Update script.js

--- Delete DMS Remove Code Page/script.js
+++ Delete DMS Remove Code Page/script.js
@@ -9,11 +9,25 @@ document.addEventListener('DOMContentLoaded', function() {
        outputText.textContent = cleanedText;
    });
 
+   outputText.addEventListener('keydown', function(event) {
+       if ((event.ctrlKey || event.metaKey) && event.key === 'a') {
+           event.preventDefault();
+           selectElementContents(outputText);
+       }
+   });
+
    function cleanText(text) {
-       // Remove HTML tags and entities
        const tagRegex = /<[^>]*>/g; // Regex to match anything between < and >
        const entityRegex = /&[^;]+;/g; // Regex to match HTML entities
 
        return text.replace(tagRegex, '').replace(entityRegex, '');
+   }
+
+   function selectElementContents(element) {
+       const range = document.createRange();
+       range.selectNodeContents(element);
+       const selection = window.getSelection();
+       selection.removeAllRanges();
+       selection.addRange(range);
    }
 });
